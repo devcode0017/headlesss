403b66e00b680ab6309f284be0e19abbe26388bc
abid <abidshahriar7@gmail.com>
2022-11-06 18:13:30 +0600

    fixed date format

--- utils/functions.js
+++ utils/functions.js
@@ -1,16 +1,13 @@
+import { format } from 'date-fns';
+
 export function formatDate(date) {
-    const formatted = new Date(date).toLocaleDateString("no-NO", {
-        day: "2-digit",
-        month: "2-digit",
-        year: "2-digit"
-    });
-    return formatted;
+  return format(new Date(date), 'MMMM dd, yyyy');
 }
 
 export function metaDescription(str) {
-    return str.replace(/^(.{135}[^\s]*).*/, "$1").trim() + "...";
+  return str.replace(/^(.{135}[^\s]*).*/, '$1').trim() + '...';
 }
 
 export function removeTags(str) {
-    return str.toString().replace(/(<([^>]+)>)/gi, "");
+  return str.toString().replace(/(<([^>]+)>)/gi, '');
 }
